8eaf9c01545cf20edb1ceb5e1fb3917cf18f58b1
mercuryglory <wzhseu@163.com>
2020-07-14 22:54:09 +0800

    update

--- array/LC48.java
+++ array/LC48.java
@@ -31,7 +31,6 @@ public class LC48 {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         rotate(arr);
-        System.out.println(123);
     }
 
 }
