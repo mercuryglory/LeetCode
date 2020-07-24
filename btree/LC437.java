52991931dac9b7aa8cbe1b38b424bcf63aa51952
mercuryglory <wzhseu@163.com>
2020-07-24 08:47:22 +0800

    LC437 更正递归方式

--- btree/LC437.java
+++ btree/LC437.java
@@ -11,8 +11,8 @@ public class LC437 {
     /**
      * 把问题转化为求三部分的和：
      * 1、以当前节点作为头节点的路径数量
-     * 2、以当前节点的左孩子作为头节点的路径数量
-     * 3、以当前节点的右孩子作为头节点的路径数量
+     * 2、当前节点的左子树中满足条件的路径数量
+     * 3、当前节点的右子树中满足条件的路径数量
      */
     public static int pathSum(TreeNode root, int sum) {
         if (root == null) {
@@ -20,8 +20,8 @@ public class LC437 {
         }
 
         int rootCount = pathCount(root, sum);
-        int leftCount = pathCount(root.left, sum);
-        int rightCount = pathCount(root.right, sum);
+        int leftCount = pathSum(root.left, sum);
+        int rightCount = pathSum(root.right, sum);
         return rootCount + leftCount + rightCount;
 
     }
@@ -38,17 +38,22 @@ public class LC437 {
     }
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(10);
-        root.left = new TreeNode(5);
-        root.left.left = new TreeNode(3);
-        root.left.right = new TreeNode(2);
-        root.left.left.left = new TreeNode(3);
-        root.left.left.right = new TreeNode(-2);
-        root.left.right.right = new TreeNode(1);
-        root.right = new TreeNode(-3);
-        root.right.right = new TreeNode(11);
+//        TreeNode root = new TreeNode(10);
+//        root.left = new TreeNode(5);
+//        root.left.left = new TreeNode(3);
+//        root.left.right = new TreeNode(2);
+//        root.left.left.left = new TreeNode(3);
+//        root.left.left.right = new TreeNode(-2);
+//        root.left.right.right = new TreeNode(1);
+//        root.right = new TreeNode(-3);
+//        root.right.right = new TreeNode(11);
 
-        System.out.println(pathSum(root, 8));
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(3);
+        root.right.right.right = new TreeNode(4);
+
+        System.out.println(pathSum(root, 3));
 
     }
 
