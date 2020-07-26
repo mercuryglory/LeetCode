6b9ab6f8a5f89c3c83c8db42986056062ae577e6
mercuryglory <wzhseu@163.com>
2020-07-26 21:16:14 +0800

    LC02

---
+++ linkedlist/LC02.java
@@ -0,0 +1,74 @@
+package linkedlist;
+
+/**
+ * created by mercury on 2020-07-26
+ *
+ * 两数相加
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ */
+
+public class LC02 extends BaseNode {
+
+    /**
+     * 题目的逆序指的是从最低位到最高位...
+     *
+     * 1、如果两个链表的长度不一样，那么短链表上为空的点补0
+     * 2、每一位计算时都要考虑上一位的进位问题，进位以carry表示，则carry=0或1，因为两位相加最大=9+9+1=19
+     * 3、两个链表全部遍历完毕后，进位位1，则在新链表末尾添加节点1
+     *
+     */
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(-1);
+        ListNode cur = pre;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.value;
+            int y = l2 == null ? 0 : l2.value;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+
+            cur = cur.next;
+            if(l1!=null){
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        if (carry == 1) {
+            cur.next = new ListNode(1);
+        }
+
+        return pre.next;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode res = addTwoNumbers(l1, l2);
+        printList(res);
+    }
+
+}
