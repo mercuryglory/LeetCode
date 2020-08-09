4c2185b0981e12e98dc847428898a9ba7a5b2a2d
mercuryglory <wzhseu@163.com>
2020-08-09 16:49:21 +0800

    backtrace

--- backtrace/LC22.java
+++ backtrace/LC22.java
@@ -1,4 +1,4 @@
-package string;
+package backtrace;
 
 import java.util.ArrayList;
 import java.util.List;
--- backtrace/LC39.java
+++ backtrace/LC39.java
@@ -1,4 +1,4 @@
-package array;
+package backtrace;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
