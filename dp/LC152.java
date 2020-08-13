1335272cf30a8bcacf6297bcd7258ad0e9be1752
mercuryglory <wzhseu@163.com>
2020-08-13 23:57:33 +0800

    LC152

---
+++ dp/LC152.java
@@ -0,0 +1,47 @@
+package dp;
+
+/**
+ * created by mercury on 2020-08-13
+ *
+ * 乘积最大子数组
+ *
+ * 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: [2,3,-2,4]
+ * 输出: 6
+ * 解释: 子数组 [2,3] 有最大乘积 6。
+ * 示例 2:
+ *
+ * 输入: [-2,0,-1]
+ * 输出: 0
+ * 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ *
+ *
+ */
+
+public class LC152 {
+
+    public static int maxProduct(int[] nums) {
+        int res = nums[0];
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int mx = max;
+            int mn = min;
+            max = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
+            min = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
+            res = Math.max(max, res);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, -5, -2, -4, 3};
+        System.out.println(maxProduct(nums));
+
+    }
+}
