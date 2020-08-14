c9ac1bd6b7cf7b9ccbcf36744ec8e65f26a4200f
mercuryglory <wzhseu@163.com>
2020-08-14 09:39:08 +0800

    update LC152

--- dp/LC152.java
+++ dp/LC152.java
@@ -25,16 +25,29 @@ package dp;
 
 public class LC152 {
 
+    /**
+     * 不同于{@link LC53}，这里的fmax(i)=max{fmax(i-1)*a[i],a[i]}是错误的，因为乘积有负负得正的情况，
+     * 比如{5,6,-3,4,-3}，当前位置的最优解不一定是由前一个位置的最优解转移得到
+     *
+     * 但可以通过正负情况分类讨论
+     * 如果当前位置时负数，我们期望以它前一个位置结尾的某个段的乘积也是负数，并且负数越小，乘积越大
+     * 反过来，希望它前一个位置结尾的某个段的乘积是正数，并且尽可能大
+     *
+     * fmax=max{fmax(i-1)*a[i],a[i],fmin(i-1)*a[i]}
+     * fmin=min{fmax(i-1)*a[i],a[i],fmin(i-1)*a[i]}
+     *
+     * 由于第i个状态只和第i-1个状态有关，可以用两个变量维护i-1时刻的状态
+     */
     public static int maxProduct(int[] nums) {
         int res = nums[0];
-        int max = nums[0];
-        int min = nums[0];
+        int fmax = nums[0];
+        int fmin = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            int mx = max;
-            int mn = min;
-            max = Math.max(mx * nums[i], Math.max(nums[i], mn * nums[i]));
-            min = Math.min(mn * nums[i], Math.min(nums[i], mx * nums[i]));
-            res = Math.max(max, res);
+            int max = fmax;
+            int min = fmin;
+            fmax = Math.max(max * nums[i], Math.max(nums[i], min * nums[i]));
+            fmin = Math.min(max * nums[i], Math.min(nums[i], min * nums[i]));
+            res = Math.max(fmax, res);
         }
         return res;
     }
