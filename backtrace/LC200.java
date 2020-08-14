26dbe6f41f6677e535b5a3617de5bd81417bd9bb
mercuryglory <wzhseu@163.com>
2020-08-14 11:41:31 +0800

    LC200

---
+++ backtrace/LC200.java
@@ -0,0 +1,82 @@
+package backtrace;
+
+/**
+ * created by mercury on 2020-08-14
+ *
+ * 岛屿数量
+ *
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+ *
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * [
+ * ['1','1','1','1','0'],
+ * ['1','1','0','1','0'],
+ * ['1','1','0','0','0'],
+ * ['0','0','0','0','0']
+ * ]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入:
+ * [
+ * ['1','1','0','0','0'],
+ * ['1','1','0','0','0'],
+ * ['0','0','1','0','0'],
+ * ['0','0','0','1','1']
+ * ]
+ * 输出: 3
+ * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+ *
+ */
+public class LC200 {
+
+    public static int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int num = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return num;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        int row = grid.length;
+        int col = grid[0].length;
+        if (i < 0 || i >= row || j < 0 || j >= col || grid[i][j] != '1') {
+            return;
+        }
+
+        grid[i][j] = '2';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {{'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}};
+        System.out.println(numIslands(grid));
+
+    }
+}
