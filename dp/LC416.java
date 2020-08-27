c0923ad8f151aa464358a129002cd5540725e955
mercuryglory <wzhseu@163.com>
2020-08-27 23:32:06 +0800

    LC416

---
+++ dp/LC416.java
@@ -0,0 +1,77 @@
+package dp;
+
+/**
+ * created by mercury on 2020-08-27
+ *
+ * 分割等和子集
+ *
+ * 给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ *
+ * 注意:
+ *
+ * 每个数组中的元素不会超过 100
+ * 数组的大小不会超过 200
+ *
+ * 示例 1:
+ * 输入: [1, 5, 11, 5]
+ * 输出: true
+ * 解释: 数组可以分割成 [1, 5, 5] 和 [11].
+ *  
+ *
+ * 示例 2:
+ * 输入: [1, 2, 3, 5]
+ * 输出: false
+ * 解释: 数组不能分割成两个元素和相等的子集.
+ *
+ */
+
+public class LC416 {
+
+    /**
+     * 初始版本 待优化
+     */
+    public static boolean canPartition(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if ((sum & 1) == 1) {
+            return false;
+        }
+
+        int target = sum / 2;
+        boolean[][] dp = new boolean[len][target + 1];
+        if (nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j <= target; j++) {
+                if (j == nums[i]) {
+                    dp[i][j] = true;
+                    continue;
+                }
+                if (nums[i] < j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+                }else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[len - 1][target];
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 5};
+        System.out.println(canPartition(nums));
+
+    }
+}
