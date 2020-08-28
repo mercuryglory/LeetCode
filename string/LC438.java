609c13ada7c1567e05b45713eb6ceb5d97aa1c87
mercuryglory <wzhseu@163.com>
2020-08-28 23:19:53 +0800

    LC438

---
+++ string/LC438.java
@@ -0,0 +1,96 @@
+package string;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * created by mercury on 2020-08-28
+ *
+ * 找到字符串中所有字母异位词
+ *
+ * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
+ *
+ * 字符串只包含小写英文字母，并且字符串 s 和 p 的长度都不超过 20100。
+ *
+ * 说明：
+ *
+ * 字母异位词指字母相同，但排列不同的字符串。
+ * 不考虑答案输出的顺序。
+ *
+ * 示例 1:
+ * 输入:
+ * s: "cbaebabacd" p: "abc"
+ * 输出:
+ * [0, 6]
+ *
+ * 解释:
+ * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的字母异位词。
+ * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的字母异位词。
+ *
+ * 示例 2:
+ * 输入:
+ * s: "abab" p: "ab"
+ * 输出:
+ * [0, 1, 2]
+ *
+ * 解释:
+ * 起始索引等于 0 的子串是 "ab", 它是 "ab" 的字母异位词。
+ * 起始索引等于 1 的子串是 "ba", 它是 "ab" 的字母异位词。
+ * 起始索引等于 2 的子串是 "ab", 它是 "ab" 的字母异位词。
+ *
+ */
+
+public class LC438 {
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int valid = 0;
+        List<Integer> res = new ArrayList<>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            //进行窗口内数据的一系列更新
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            //判断左侧窗口是否要收缩
+            while (right - left >= p.length()) {
+                //当窗口符合条件时，把起始索引加入res
+                if (valid == need.size()) {
+                    res.add(left);
+                }
+                char d = s.charAt(left);
+                left++;
+                //进行窗口内数据的一系列更新
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.getOrDefault(d, 0) - 1);
+                }
+
+            }
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "cbaebabacd";
+        String p = "abc";
+        System.out.println(findAnagrams(s, p));
+    }
+}
