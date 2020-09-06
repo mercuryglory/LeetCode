c872d8a034cf87bf84e6272c76065d30a7ef7bd0
mercuryglory <wzhseu@163.com>
2020-09-06 10:28:08 +0800

    update LC10

--- dp/LC10.java
+++ dp/LC10.java
@@ -66,7 +66,16 @@ public class LC10 {
      * 两个空字符是匹配的，因此初始化dp[0][0]=true
      *
      * 如果我们已经得出了前i-1个和前j-1个字符的匹配情况dp[i-1][j-1]，只要对p中特殊的'.'和'*'多加注意
+     * 因为下标从0开始，前i-1个其实是[0,i-2]的元素，因此遍历判断匹配当前字符串时都以i-1比较
      *
+     * 匹配逻辑是，如果p[j-1]=='.'，代表任意字符，相当于s[i-1]==p[j-1]。否则比较s[i-1]和p[j-1]
+     * 然后分情况讨论：
+     * 1、如果p[j-1]不等于'*'，那么如果匹配上了，则dp[i][j]取决于dp[i-1][j-1]
+     * 2、如果p[j-1]等于'*'，又分两种情况，比较p[j-1]前面的字符p[j-2]
+     *    若p[j-2]能与当前s[i-1]匹配，则dp[i][j]取决于dp[i-1][j]。同时若dp[i-1][j]=false,我们
+     *    还可以跳过p[j-1]不用他了，直接看s的前i个字符能否与p的前j-2个字符匹配，即dp[i][j-2]
+     *
+     *    若不匹配，就是p[j-1]使用0次，还是看dp[i][j-2]
      *
      */
     public static boolean isMatch(String s, String p) {
@@ -84,10 +93,7 @@ public class LC10 {
                 } else {
                     //这个地方没做校验，认为题目能保证p的合法性，因为j=1时p[0]!='*'，否则*前面没有可以匹配的字符
                     //所以到这里j>=2，不会造成数组越界
-                    dp[i][j] = dp[i][j - 2];
-                    if (match(s, p, i, j - 1)) {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j];
-                    }
+                    dp[i][j] = match(s, p, i, j - 1) ? dp[i - 1][j] || dp[i][j - 2] : dp[i][j - 2];
                 }
             }
         }
@@ -97,6 +103,7 @@ public class LC10 {
     }
 
     public static boolean match(String s, String p, int i, int j) {
+        //字符串s中0个字符肯定和p中的j(j>=1)个字符不匹配
         if (i == 0) {
             return false;
         }
