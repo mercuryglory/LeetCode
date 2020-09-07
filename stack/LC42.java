94e885a5db9309e665317340206769fdeac12cc0
mercuryglory <wzhseu@163.com>
2020-09-07 22:20:35 +0800

    LC42

---
+++ stack/LC42.java
@@ -0,0 +1,46 @@
+package stack;
+
+/**
+ * created by mercury on 2020-09-07
+ *
+ * 接雨水
+ *
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ *
+ * 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+ * 感谢 Marcos 贡献此图。
+ *
+ * 示例:
+ *
+ * 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出: 6
+ *
+ */
+public class LC42 {
+
+    public static int trap(int[] height) {
+        int res = 0;
+        int len = height.length;
+        for (int i = 1; i < len - 1; i++) {
+            int maxLeft = 0;
+            int maxRight = 0;
+            for (int j = i; j >= 0; j--) {
+                maxLeft = Math.max(maxLeft, height[j]);
+            }
+            for (int j = i; j < len; j++) {
+                maxRight = Math.max(maxRight, height[j]);
+            }
+            res += Math.min(maxLeft, maxRight) - height[i];
+        }
+        
+        return res;
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(trap(height));
+    }
+}
