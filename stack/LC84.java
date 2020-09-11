f09d575244b75bab8bf0ef1e09ac1668646fe113
mercuryglory <wzhseu@163.com>
2020-09-11 12:12:49 +0800

    update LC84

--- stack/LC84.java
+++ stack/LC84.java
@@ -18,7 +18,17 @@ import java.util.Stack;
 
 public class LC84 {
 
+    /**
+     * 基础的解法是，对于每一个柱子，以当前柱子的高作为矩形的高，我们要找到它的左右边界，即向左找到第一个高度小于
+     * 当前柱体高度，向右找到第一个高度小于当前柱体高度
+     * <p>
+     * 优化思路是用单调栈，栈中下一个柱体就是左边第一个高度小于自身的柱体
+     * 用栈遍历每个柱体，若当前柱体高度>=栈顶柱体的高度，就直接将当前柱体入栈
+     *
+     */
     public static int largestRectangleArea(int[] heights) {
+        //在柱体数组的头和尾加了两个高度为0的柱体，很妙，可以简化逻辑。因为开始的时候栈为空，而遍历到最后如果栈不为空需要
+        //弹出栈中所有元素
         int[] arr = new int[heights.length + 2];
         for (int i = 1; i < heights.length + 1; i++) {
             arr[i] = heights[i - 1];
@@ -27,7 +37,8 @@ public class LC84 {
         Stack<Integer> stack = new Stack<>();
         int res = 0;
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(stack);
+            //找到了右边第一个高度小于栈顶柱体高度的柱体，右边界确定，再向左找到所有符合条件的柱体。因为左边界已经确定，
+            //只要是大于当前柱体高度的柱体都要计算，然后比较面积
             while (!stack.isEmpty() && arr[i] < arr[stack.peek()]) {
                 int top = stack.pop();
                 res = Math.max(res, (i - stack.peek() - 1) * arr[top]);
