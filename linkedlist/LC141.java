28cb7ffeec9ee06210495d0967c85ea667d7b66c
mercuryglory <wzhseu@163.com>
2022-09-17 22:47:20 +0800

    update

--- linkedlist/LC141.java
+++ linkedlist/LC141.java
@@ -2,8 +2,29 @@ package linkedlist;
 
 /**
  * created by mercury on 2020-07-20
- *
+ * <p>
  * 环形链表
  */
-public class LC141 {
+public class LC141 extends BaseNode {
+
+    //快慢指针，如果无环，快指针肯定先到尾部
+    private static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 0, -4};
+        ListNode circle = generateCircle(arr, -1);
+        System.out.println(hasCycle(circle));
+    }
 }
