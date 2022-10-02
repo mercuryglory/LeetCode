79e99864934adae5133117c4f5f1870c91f47fc2
mercuryglory <wzhseu@163.com>
2022-10-02 22:30:50 +0800

    LC206 反转链表

--- linkedlist/LC206.java
+++ linkedlist/LC206.java
@@ -1,8 +1,29 @@
 package linkedlist;
 
+
 /**
  * created by mercury on 2020-07-17
  * 反转链表
  */
-public class LC206 {
+public class LC206 extends BaseNode {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode node = generateListNode(5);
+        ListNode reverse = reverseList(node);
+        printList(reverse);
+    }
+
 }
