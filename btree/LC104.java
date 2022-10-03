d6812060fd1d1f9743003a2ee65e95d921f05ce6
mercuryglory <wzhseu@163.com>
2022-10-03 21:47:03 +0800

    LC104 二叉树的最大深度

--- btree/LC104.java
+++ btree/LC104.java
@@ -2,8 +2,24 @@ package btree;
 
 /**
  * created by mercury on 2020-07-18
- *
+ * <p>
  * 二叉树的最大深度
  */
-public class LC104 {
+public class LC104 extends BaseTreeNode {
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+
+    public static void main(String[] args) {
+        Integer[] arr = {3, 9, 20, null, null, 15, 7};
+        TreeNode root = generateTreeNode(arr);
+        System.out.println(maxDepth(root));
+    }
 }
