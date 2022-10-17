e734e2883b90ee57c4c913979468d314b5758cc3
mercuryglory <wzhseu@163.com>
2022-10-17 22:17:57 +0800

    update

--- backtrace/LC79.java
+++ backtrace/LC79.java
@@ -1,8 +1,5 @@
 package backtrace;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * created by mercury on 2020-08-09
  *
@@ -38,68 +35,45 @@ import java.util.List;
 
 public class LC79 {
 
-    private String word;
-    private char[][] board;
-    private boolean[][] flag;
-    private int row;
-    private int col;
-    private int[][] direction = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-
     /**
      * 二维平面中偏移量数组是常用的技巧，该问题中4个偏移顺序的次序无关紧要
      * 这里的回溯指判断不匹配时，二维平面上的指针后退，如何重置状态很重要
      * 题解配图：https://leetcode-cn.com/problems/word-search/solution/zai-er-wei-ping-mian-shang-shi-yong-hui-su-fa-pyth/
      */
+
     public boolean exist(char[][] board, String word) {
-        row = board.length;
-        if (row == 0) {
-            return false;
-        }
-
-        col = board[0].length;
-        flag = new boolean[row][col];
-        this.word = word;
-        this.board = board;
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (dfs(i, j, 0)) {
+        int m = board.length;
+        int n = board[0].length;
+        int[][] direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        boolean[][] flag = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, i, j, word, direction, flag, 0)) {
                     return true;
                 }
             }
         }
-
         return false;
-
     }
 
-    private boolean dfs(int i, int j, int start) {
+    public boolean dfs(char[][] board, int i, int j, String word, int[][] direction, boolean[][] flag, int start) {
         if (start == word.length() - 1) {
             return word.charAt(start) == board[i][j];
         }
         if (word.charAt(start) == board[i][j]) {
             flag[i][j] = true;
-            //开始四个方向dfs
-            for (int k = 0; k <4 ; k++) {
-                int newX = i + direction[k][0];
-                int newY = j + direction[k][1];
-                if (isInArea(newX, newY) && !flag[newX][newY]) {
-                    if (dfs(newX, newY, start + 1)) {
+            for (int k = 0; k < 4; k++) {
+                int x = i + direction[k][0];
+                int y = j + direction[k][1];
+                if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && !flag[x][y]) {
+                    if (dfs(board, x, y, word, direction, flag, start + 1)) {
                         return true;
                     }
                 }
             }
-
-
-            //不是一条dfs路径上的节点可以重复访问
             flag[i][j] = false;
-
         }
         return false;
-    }
-
-    private boolean isInArea(int x, int y) {
-        return x >= 0 && x < row && y >= 0 && y < col;
     }
 
 
@@ -108,7 +82,7 @@ public class LC79 {
                 {'S', 'F', 'C', 'S'},
                 {'A', 'D', 'E', 'E'}};
         LC79 obj = new LC79();
-        String word = "ABCCED";
+        String word = "SEE";
         System.out.println(obj.exist(board, word));
 
     }
